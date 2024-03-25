444594e1c838a229277663abd08d6a4da0b577da
jurgenvrapi97 <jurgenvrapi@gmail.com>
2024-03-25 18:28:32 +0100

    last update

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,8 +1,18 @@
 package org.example;
+import dao.DistributoreDAO;
+import entities.Distributore;
+import enums.Stato;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Application {
-
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("MagicTrasportSrl");
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
+        DistributoreDAO distributoreDAO = new DistributoreDAO(em);
+
+        Distributore distributore = new Distributore(Stato.ATTIVO, "marche", 39, null,null);
+        distributoreDAO.saveDistributore(distributore);
     }
 }
