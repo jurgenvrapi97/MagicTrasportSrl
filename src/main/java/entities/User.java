05da13950612b102bf462c87b5972204c78b9d4e
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-03-25 17:07:10 +0100

    completes-user-card-with-reletionship

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,9 +1,70 @@
 package entities;
 
+import jakarta.persistence.OneToOne;
+
 public class User {
+    //attributes
+    //@Id
+// @GeneratedValue(strategy = GenerationType.IDENTITY)
+//  @Column(name="user_id")
     private long id;
     private String name;
     private String surname;
     private int age;
+//    @OneToOne(mappedBy = "user")
+//    private Card card;
+    //constructors
 
+    public User() {
+    }
+
+    public User(String name, String surname, int age) {
+        this.setName(name);
+        this.setSurname(surname);
+        this.setAge(age);
+    }
+
+    //getters
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    //setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    //methods
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
