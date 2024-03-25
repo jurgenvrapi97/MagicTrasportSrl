463948440f4f30a9ecbf4c2a8eb27842a1e521a9
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-03-25 16:37:43 +0100

    basic-user-and card-classes

---
+++ src/main/java/entities/Card.java
@@ -0,0 +1,8 @@
+package entities;
+
+
+
+public class Card {
+    private long id;
+    private LocalDate data_di_scadenza;
+}
---
+++ src/main/java/entities/User.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class User {
+    private long id;
+    private String name;
+    private String surname;
+    private int age;
+
+}
