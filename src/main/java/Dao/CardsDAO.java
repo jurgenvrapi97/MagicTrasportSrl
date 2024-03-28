d94d74c4ff848c7778eab189359805fd24ba32d0
jurgenvrapi97 <jurgenvrapi@gmail.com>
2024-03-28 10:42:42 +0100

    commit

--- src/main/java/Dao/CardsDAO.java
+++ src/main/java/Dao/CardsDAO.java
@@ -1,13 +1,16 @@
 package Dao;
 
 import entities.Card;
-import entities.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 
-public class CardsDAO {
+import java.time.LocalDate;
+
+public class CardsDao {
     EntityManager em;
-    public CardsDAO(EntityManager em) {
+    public CardsDao(EntityManager em) {
         this.em = em;
     }
 
@@ -23,5 +26,18 @@ public class CardsDAO {
         em.persist(card);
         transaction.commit();
         System.out.println("Carta"+ card.toString()+" salvata");
+    }
+    public  Card findIsExpired(long card_id){
+        TypedQuery<Card>query=em.createNamedQuery("findIsExpired", Card.class);
+        query.setParameter("id",card_id);
+        query.setParameter("today", LocalDate.now());
+        try{ Card found=query.getSingleResult();
+            System.out.println("trovata card non scaduta: "+found);
+            return found;}
+        catch (NoResultException ex){
+           System.out.println("Carta inesistente o scaduta");
+            return null;
+        }
+
     }
 }
